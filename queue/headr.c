edd8a0fab74ebecb7be95e910b7fe99dac2695b7
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2024-11-02 21:12:23 +0200

    Update headr.c

--- queue/headr.c
+++ queue/headr.c
@@ -1,15 +1,15 @@
 //
-// Created by mahmo on 10/29/2024.
+// Created by mahmo on 11/2/2024.
 //
 #ifndef UNTITLED2_QUEUE_H
 #define UNTITLED2_QUEUE_H
 #define N 5
 typedef int type;
 typedef struct Queue {
-   int size;
-   int insert;
-   int erase;
-   type items[N];
+    int size;
+    int insert;
+    int erase;
+    type items[N];
 }queue;
 void creatqueue(queue *s);
 int size( queue *s);
@@ -18,6 +18,5 @@ int isempty( queue *s);
 void push(queue *s,type item);
 type pop(queue *s);
 type top(queue *s);
-/*void copy( stack *c,stack *s);
-void traverse(stack *s,void(*pf)(type e));*/
+void traverse(queue *q, void (*pf)(type e));
 #endif //UNTITLED2_QUEUE_H
