d681033c60421d226e47425fbdcc929259ab5ffa
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2024-11-22 12:35:05 +0200

    Update src.c

--- linked-list/src.c
+++ linked-list/src.c
@@ -1,99 +1,94 @@
-//
-// Created by mahmo on 2024-11-22.
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
-void creatlist(list *l){
-    l->head=NULL;
-    l->size=0;
+
+void creatlist(list *l) {
+    l->head = NULL;
+    l->size = 0;
 }
-int isfull(list *l){
+
+int isfull(list *l) {
     return 0;
 }
-void insert(list *l,type item,int pos){
-    if(pos>l->size+1 || pos<1){
-        printf("invalid posstion");
+
+void insert(list *l, type item, int pos) {
+    if (pos > l->size + 1 || pos < 1) {
+        printf("Invalid position\n");
         return;
     }
-    node *pn=(node*) malloc(sizeof(node));
-    pn->info=item;
-    if(l->size==0){
-        l->head=pn;
-        pn->next=l->head;
-    }else{
-        node *pt=l->head;
-        for (int i = 0; i <pos-1 ; ++i) {
-            pt=pt->next;
+    node *pn = (node *)malloc(sizeof(node));
+    pn->info = item;
+    if (pos == 1) { 
+        pn->next = l->head;
+        l->head = pn;
+    } else { 
+        node *pt = l->head;
+        for (int i = 1; i < pos - 1; ++i) {
+            pt = pt->next;
         }
-        pn->next=pt->next;
-        pt->next=pn;
+        pn->next = pt->next;
+        pt->next = pn;
     }
     l->size++;
 }
+
 type peak(list *l) {
     if (l->head == NULL) {
         printf("List is empty!\n");
         return 0;
     }
-    node *pt = l->head;
-    for (int i = 0; i <l->size-1 ; ++i) {
-        pt=pt->next;
-    }
-    return pt->info;
+    return l->head->info;
 }
-int count(list *l,type elemnt){
-    int cnt=0;
-    node *pt=l->head;
-    for (int i = 0; i <l->size ; ++i) {
-        if(pt->info==elemnt) cnt++;
-        pt=pt->next;
+
+int count(list *l, type elemnt) {
+    int cnt = 0;
+    node *pt = l->head;
+    while (pt != NULL) {
+        if (pt->info == elemnt)
+            cnt++;
+        pt = pt->next;
     }
     return cnt;
 }
+
 type erase(list *l, int position) {
     if (l->head == NULL) {
         printf("Error: List is empty.\n");
         return 0;
     }
 
-    if (position < 0) {
+    if (position < 0 || position >= l->size) {
         printf("Error: Invalid position.\n");
         return 0;
     }
 
     node *current = l->head;
     type elem;
-    if (position == 0) {
+    if (position == 0) { 
         elem = current->info;
         l->head = current->next;
         free(current);
-        return elem;
-    }
-    node *prev = NULL;
-    for (int i = 0; i < position; i++) {
-        if (current == NULL) {
-            printf("Error: Position out of range.\n");
-            return 0;
+    } else {
+        node *prev = NULL;
+        for (int i = 0; i < position; i++) {
+            prev = current;
+            current = current->next;
         }
-        prev = current;
-        current = current->next;
+        elem = current->info;
+        prev->next = current->next;
+        free(current);
     }
-
-    if (current == NULL) {
-        printf("Error: Position out of range.\n");
-        return 0;
-    }
-
-    elem = current->info;
-    prev->next = current->next;
-    free(current);
-
+    l->size--;
     return elem;
 }
-void joinlist(list *l1,list *l2){
+
+void joinlist(list *l1, list *l2) {
     if (l1->head == NULL) {
         l1->head = l2->head;
+        l1->size = l2->size;
+        return;
+    }
+    if (l2->head == NULL) {
         return;
     }
     node *pt = l1->head;
@@ -101,4 +96,5 @@ void joinlist(list *l1,list *l2){
         pt = pt->next;
     }
     pt->next = l2->head;
+    l1->size += l2->size;
 }
