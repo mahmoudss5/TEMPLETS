ea6ac63a2ce7d0116646ef441cb6cd13d18d824a
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2024-12-13 03:19:13 +0200

    Create SRC.c

---
+++ BST/SRC.c
@@ -0,0 +1,125 @@
+#include "bst..h"
+#include <stdio.h>
+#include <stdlib.h>
+void create(bst *t) {
+    t->t = NULL;
+    t->size = 0;
+}
+
+void insert(bst *t, type val) {
+    node *pn = (node *)malloc(sizeof(node));
+    if (!pn) {
+        printf("Error: Memory allocation failed.\n");
+        return;
+    }
+    pn->value = val;
+    pn->left = NULL;
+    pn->right = NULL;
+
+    if (t->t == NULL) {
+        t->t = pn;
+    } else {
+        node *cur = t->t, *prev = NULL;
+
+        while (cur) {
+            prev = cur;
+            if (val > cur->value) {
+                cur = cur->right;
+            } else {
+                cur = cur->left;
+            }
+        }
+
+        if (val > prev->value) {
+            prev->right = pn;
+        } else {
+            prev->left = pn;
+        }
+    }
+    t->size++;
+    printf("Insertion of value %d is done.\n", val);
+}
+type findMin(node *root) {
+    if (root == NULL) {
+        printf("Error: Tree is empty!\n");
+        exit(EXIT_FAILURE);
+    }
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root->value;
+}
+
+void erase(bst *t, type val) {
+    if (t->t == NULL) {
+        printf("Tree is empty.\n");
+        return;
+    }
+
+    int found = 0;
+    t->t = eraseNode(t->t, val, &found);
+
+    if (found) {
+        printf("Value %d deleted successfully.\n", val);
+        t->size--;
+    } else {
+        printf("Value %d not found in the tree.\n", val);
+    }
+}
+
+node* eraseNode(node *root, type val, int *found) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (val < root->value) {
+        root->left = eraseNode(root->left, val, found);
+    } else if (val > root->value) {
+        root->right = eraseNode(root->right, val, found);
+    } else {
+        *found = 1;
+        if (root->left == NULL) {
+            node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        type minVal = findMin(root->right);
+        root->value = minVal;
+        root->right = eraseNode(root->right, minVal, found);
+    }
+    return root;
+}
+
+int find(bst *t, type val) {
+    node *cur = t->t;
+
+    while (cur) {
+        if (val == cur->value) {
+            return 1;
+        } else if (val < cur->value) {
+            cur = cur->left;
+        } else {
+            cur = cur->right;
+        }
+    }
+
+    return 0;
+}
+
+
+int isfull(bst *t) {
+    return 0;
+}
+
+int isempty(bst *t) {
+    return t->t == NULL;
+}
+
+int size(bst *t) {
+    return t->size;
+}
