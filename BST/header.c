b1c3723d5d74b9e8f160a9cde4644ef7d8b2bb42
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2024-12-13 03:19:48 +0200

    Create header.c

---
+++ BST/header.c
@@ -0,0 +1,29 @@
+// yaraaaaaaaaaaaaaaaaaaaaaaaab
+#ifndef UNTITLED6_BST_H
+#define UNTITLED6_BST_H
+
+#define type int
+
+typedef struct Node {
+    type value;
+    struct Node *left;
+    struct Node *right;
+} node;
+
+typedef struct BST {
+    node *t;
+    int size;
+} bst;
+
+
+void create(bst *t);
+void insert(bst *t, type val);
+void erase(bst *t, type val);
+int find(bst *t, type val);
+int isfull(bst *t);
+int isempty(bst *t);
+node* eraseNode(node *root, type val, int *found);
+type findMin(node *root);
+int size(bst *t);
+
+#endif // UNTITLED6_BST_H
