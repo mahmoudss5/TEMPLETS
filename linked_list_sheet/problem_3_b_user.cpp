5102848fd1ce190d2522c026dc06b4c4ace72552
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2025-06-12 20:45:14 +0300

    Delete linked_list_sheet directory

--- linked_list_sheet/problem_3_b_user.cpp
+++
@@ -1,9 +0,0 @@
-void join(list *l, list *l2) {
-    int sz = size(l2);
-    int sz1 = size(l);
-    for (int i = 0; i < sz; ++i) {
-        type item = peak(l2, i);
-        insert(l, item, sz1);
-        sz1++;
-    }
-}
