d0560b5d4ade40dbeabc36946db8a020f6baff1a
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2025-07-28 16:31:40 +0300

    Delete seive.c

--- seive.c
+++
@@ -1,9 +0,0 @@
-bool flag[N];
-map<int, bool> mp;
-void getprime() {
-    for (ll i = 2; i < N; i++) {
-        if (flag[i] == 1) continue;
-        mp[i] = 1;
-        for (ll j = i * i; j < N; j += i) flag[j] = 1;
-    }
-}
